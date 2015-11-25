4073a613c13c51f1077f74ec8ee14a9984490018
tmgree <tmgree@outlook.com>
2015-11-25 16:51:57 +0000

    7

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -4,6 +4,7 @@ require_relative 'bike'
 class DockingStation
   attr_reader :bike
 def release_bike
+  fail "no bikes" unless @bike
   @bike
 end
 def dock(bike)
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -6,7 +6,14 @@ it {is_expected.to respond_to :release_bike}
 
 
 it {is_expected.to respond_to(:dock).with(1).argument}
+it 'should allow a bike to dock' do
+  bike=Bike.new
+  expect(subject.dock(bike)).to eq bike
+end
 
+it 'should respond to method bike' do
+  expect(subject).to respond_to :bike
+end
 
 it "Should show a docked bike" do
 bike = Bike.new
@@ -20,6 +27,11 @@ describe '#release_bike' do
     bike=Bike.new
     subject.dock(bike)
     expect(subject.release_bike).to eq bike
+
+
+  end
+  it 'shows an error if station is empty' do
+    expect{ subject.release_bike}.to raise_error("no bikes")
   end
 end
 
