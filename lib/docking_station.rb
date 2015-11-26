11b6860c4d2e66190a2444dfc78e19cd793731c0
gareth4192 <gareth-123@live.com>
2015-11-26 12:24:54 +0000

    challange 15 completed

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,13 +2,29 @@ require_relative 'bike'
 
 
 class DockingStation
-  attr_reader :bike
-def release_bike
-  fail "no bikes" unless @bike
-  @bike
+  attr_reader :bikes
+
+def initialize
+  @bikes=[]
 end
+
+def release_bike
+  fail "no bikes" if empty?
+  @bikes.pop
+end
+
 def dock(bike)
-@bike=bike
+  fail "error full" if full?
+  @bikes << bike
+end
+
+private
+def empty?
+  @bikes.size == 0 ? true : false
+end
+
+def full?
+  @bikes.size >= 20 ? true : false
 end
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,40 +1,45 @@
 require 'docking_station'
 
 describe DockingStation do
-it {is_expected.to respond_to :release_bike}
+it {is_expected.to respond_to :release_bike} #
 
 
 
-it {is_expected.to respond_to(:dock).with(1).argument}
+it {is_expected.to respond_to(:dock).with(1).argument} #
+
 it 'should allow a bike to dock' do
-  bike=Bike.new
-  expect(subject.dock(bike)).to eq bike
+  bike = Bike.new
+  subject.dock(bike)
+  expect(subject.release_bike).to eq bike
 end
 
 it 'should respond to method bike' do
-  expect(subject).to respond_to :bike
+  expect(subject).to respond_to :bikes
 end
 
 it "Should show a docked bike" do
-bike = Bike.new
-subject.dock(bike)
-expect(subject.dock(bike)).to eq bike
+  bike = Bike.new
+  subject.dock(bike)
+  expect(subject.release_bike).to eq bike
 end
 
 
 describe '#release_bike' do
   it 'releases a bike' do
-    bike=Bike.new
+    bike = Bike.new
     subject.dock(bike)
     expect(subject.release_bike).to eq bike
-
-
   end
+
   it 'shows an error if station is empty' do
     expect{ subject.release_bike}.to raise_error("no bikes")
   end
-end
 
+  it 'shows an error if station is empty' do
+    20.times {subject.dock Bike.new}
+    expect{ subject.dock(Bike.new)}.to raise_error("error full")
+  end
+end
 
 
 
